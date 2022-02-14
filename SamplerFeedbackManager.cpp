9d38f4daeea3d9ca63c8063a1288be4f3b22deb0
Vishwas Srivastava <friendsandvis@gmail.com>
2022-02-14 11:22:40 +0530

    added some useful classes in sampler feedback manager to represent a basic structure.

--- SamplerFeedbackManager.cpp
+++ SamplerFeedbackManager.cpp
@@ -1,1 +1,16 @@
 #include"SamplerFeedbackManager.h"
+
+
+DX12FeedBackUnit::DX12FeedBackUnit()
+{
+}
+
+DX12FeedBackUnit::~DX12FeedBackUnit()
+{
+}
+
+
+void SamplerFeedbackTexture::Init()
+{
+
+}
--- SamplerFeedbackManager.h
+++ SamplerFeedbackManager.h
@@ -1,3 +1,19 @@
 #pragma once
 #include"DXUtils.h"
 #include"DX12Resource.h"
+
+class SamplerFeedbackTexture :public DX12Resource
+{
+	void Init();
+};
+
+class DX12FeedBackUnit
+{
+public:
+	DX12FeedBackUnit();
+	~DX12FeedBackUnit();
+
+private:
+	SamplerFeedbackTexture m_feedbacktex;
+};
+
