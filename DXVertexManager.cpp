a1fd3801e4e6b73dc08f3a78caa9816101231a96
Vishwas Srivastava <friendsandvis@gmail.com>
2022-03-31 17:23:11 +0530

    added various vertex stuctures( commonly used).

---
+++ DXVertexManager.cpp
@@ -0,0 +1,99 @@
+#include"DXVertexManager.h"
+
+void DXVertexManager::BuildDefaultInputelementdesc(std::vector< D3D12_INPUT_ELEMENT_DESC>& outinputelementdescs, VertexType vtype)
+{
+	switch (vtype)
+	{
+	case VertexType::VERTEXVERSION0:
+	{
+		{
+		D3D12_INPUT_ELEMENT_DESC element1 = {};
+		element1.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+		element1.InputSlot = 0;
+		element1.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+		element1.AlignedByteOffset = 0;
+		element1.InstanceDataStepRate = 0;
+		element1.SemanticName = "POS";
+		element1.SemanticIndex = 0;
+		outinputelementdescs.push_back(element1);
+	}
+		{
+			D3D12_INPUT_ELEMENT_DESC element2 = {};
+			element2.Format = DXGI_FORMAT_R32G32_FLOAT;
+			element2.InputSlot = 0;
+			element2.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element2.AlignedByteOffset = 3* sizeof(float);
+			element2.InstanceDataStepRate = 0;
+			element2.SemanticName = "UV";
+			element2.SemanticIndex = 0;
+			outinputelementdescs.push_back(element2);
+		}
+	}
+	break;
+	case VertexType::VERTEXVERSION1:
+	{
+		{
+			D3D12_INPUT_ELEMENT_DESC element1 = {};
+			element1.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			element1.InputSlot = 0;
+			element1.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element1.AlignedByteOffset = 0;
+			element1.InstanceDataStepRate = 0;
+			element1.SemanticName = "POS";
+			element1.SemanticIndex = 0;
+			outinputelementdescs.push_back(element1);
+		}
+		{
+			D3D12_INPUT_ELEMENT_DESC element2 = {};
+			element2.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			element2.InputSlot = 0;
+			element2.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element2.AlignedByteOffset = 3 * sizeof(float);
+			element2.InstanceDataStepRate = 0;
+			element2.SemanticName = "POSCOL";
+			element2.SemanticIndex = 0;
+			outinputelementdescs.push_back(element2);
+		}
+	}
+	break;
+	case VertexType::VERTEXVERSION2:
+	{
+		{
+			D3D12_INPUT_ELEMENT_DESC element1 = {};
+			element1.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			element1.InputSlot = 0;
+			element1.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element1.AlignedByteOffset = 0;
+			element1.InstanceDataStepRate = 0;
+			element1.SemanticName = "POS";
+			element1.SemanticIndex = 0;
+			outinputelementdescs.push_back(element1);
+		}
+		{
+			D3D12_INPUT_ELEMENT_DESC element2 = {};
+			element2.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+			element2.InputSlot = 0;
+			element2.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element2.AlignedByteOffset = 3 * sizeof(float);
+			element2.InstanceDataStepRate = 0;
+			element2.SemanticName = "POSCOL";
+			element2.SemanticIndex = 0;
+			outinputelementdescs.push_back(element2);
+		}
+		{
+			D3D12_INPUT_ELEMENT_DESC element3 = {};
+			element3.Format = DXGI_FORMAT_R32G32_FLOAT;
+			element3.InputSlot = 0;
+			element3.InputSlotClass = D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA;
+			element3.AlignedByteOffset = 6 * sizeof(float);
+			element3.InstanceDataStepRate = 0;
+			element3.SemanticName = "UV";
+			element3.SemanticIndex = 0;
+			outinputelementdescs.push_back(element3);
+		}
+	}
+	break;
+	default:
+		break;
+	}
+}
--- DXVertexManager.h
+++ DXVertexManager.h
@@ -1,2 +1,41 @@
 #pragma once
 
+#include<DirectXMath.h>
+#include"DXUtils.h"
+using namespace DirectX;
+/*
+Define Various Vertex structures to be used in applications.
+
+*/
+
+struct Vetex_V0
+{
+	XMFLOAT3 position;
+	XMFLOAT2 uv;
+};
+
+struct VetexV1
+{
+	XMFLOAT3 position;
+	XMFLOAT3 vertexcolour;
+};
+
+struct VetexV2
+{
+	XMFLOAT3 position;
+	XMFLOAT3 vertexcolour;
+	XMFLOAT2 uv;
+};
+
+enum VertexType
+{
+	VERTEXVERSION0,
+	VERTEXVERSION1,
+	VERTEXVERSION2
+};
+
+class DXVertexManager
+{
+public:
+	static void BuildDefaultInputelementdesc(std::vector< D3D12_INPUT_ELEMENT_DESC>& outinputelementdescs, VertexType vtype);
+};
