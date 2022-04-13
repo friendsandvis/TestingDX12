5c6a6dd3b99b8e4d4ab55dafe90bf197ac3a2383
Vishwas Srivastava <friendsandvis@gmail.com>
2022-04-13 10:03:18 +0530

    simly added a sampler statement to sampler a perticular lod level in reserved res application(nothing too important just for testing).

--- shaders/reseredresource/PS.hlsl
+++ shaders/reseredresource/PS.hlsl
@@ -13,4 +13,5 @@ float4 main(Vout psin) : SV_TARGET0
 {
 	//return float4(psin.uv,0.0f,1.0f);
 	return image.Sample(simplesampler,psin.uv);
+	//return image.SampleLevel(simplesampler,psin.uv,0.0f);
 }
