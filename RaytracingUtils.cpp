fe589112423f118e99616c5151c1ed59c71363fc
Vishwas Srivastava <friendsandvis@gmail.com>
2022-05-09 20:29:48 +0530

    added a comment.

--- RaytracingUtils.cpp
+++ RaytracingUtils.cpp
@@ -5,6 +5,7 @@ void ModelAccelerationStructure::Init(Model& modeltoprocess)
 	//we need ib &vb in gpu memory.
 	assert(modeltoprocess.GetUploadMode() == ModelDataUploadMode::COPY);
 	m_rtgeometrydesc.Type = D3D12_RAYTRACING_GEOMETRY_TYPE::D3D12_RAYTRACING_GEOMETRY_TYPE_TRIANGLES;
+	//fixed in the current frame work(index is unsigned int)
 	m_rtgeometrydesc.Triangles.IndexFormat = DXGI_FORMAT_R32_UINT;
 	m_rtgeometrydesc.Triangles.IndexBuffer = modeltoprocess.GetIertexBufferGPUVirtualAddress();
 	m_rtgeometrydesc.Triangles.IndexCount = (UINT)modeltoprocess.GetIndiciesCount();
