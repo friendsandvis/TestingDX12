8caadffa9d881d9009c4f4733ec1c078d5ad60f2
Vishwas Srivastava <friendsandvis@gmail.com>
2022-05-13 18:42:46 +0530

    some more progress in rtpso building.

--- RTPSO.cpp
+++ RTPSO.cpp
@@ -8,3 +8,12 @@ RTPSO::RTPSO()
 RTPSO::~RTPSO()
 {
 }
+
+void RTPSO::Init(ComPtr<ID3D12Device5> creationdevice)
+{
+	m_desc.Type = D3D12_STATE_OBJECT_TYPE::D3D12_STATE_OBJECT_TYPE_RAYTRACING_PIPELINE;
+	m_desc.NumSubobjects = static_cast<UINT>(m_statesubobjects.size());
+	m_desc.pSubobjects = m_statesubobjects.data();
+	DXASSERT(creationdevice->CreateStateObject(&m_desc, IID_PPV_ARGS(m_stateobject.GetAddressOf())))
+	
+}
--- RTPSO.h
+++ RTPSO.h
@@ -7,7 +7,11 @@ public:
 	RTPSO();
 	~RTPSO();
 
-private:
+	void Init(ComPtr<ID3D12Device5> creationdevice);
 
+private:
+	ComPtr<ID3D12StateObject> m_stateobject;
+	D3D12_STATE_OBJECT_DESC m_desc;
+	vector<D3D12_STATE_SUBOBJECT> m_statesubobjects;
 };
 
