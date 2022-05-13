f49e9ecf39d5475f6c8da35f00f39f613bbd275a
Vishwas Srivastava <friendsandvis@gmail.com>
2022-05-13 20:53:08 +0530

    some more additions to rtpso class.

--- RTPSO.cpp
+++ RTPSO.cpp
@@ -11,9 +11,20 @@ RTPSO::~RTPSO()
 
 void RTPSO::Init(ComPtr<ID3D12Device5> creationdevice)
 {
+	//at least pipeline config state should be provided for opimization.
+	assert(m_statesubobjects.size() != 0);
 	m_desc.Type = D3D12_STATE_OBJECT_TYPE::D3D12_STATE_OBJECT_TYPE_RAYTRACING_PIPELINE;
 	m_desc.NumSubobjects = static_cast<UINT>(m_statesubobjects.size());
 	m_desc.pSubobjects = m_statesubobjects.data();
 	DXASSERT(creationdevice->CreateStateObject(&m_desc, IID_PPV_ARGS(m_stateobject.GetAddressOf())))
 	
+}
+
+void RTPSO::SetPipelineConfig(UINT maxtracerecursiondepth)
+{
+	m_rtconfig.MaxTraceRecursionDepth = maxtracerecursiondepth;
+	D3D12_STATE_SUBOBJECT subobj = {};
+	subobj.Type = D3D12_STATE_SUBOBJECT_TYPE::D3D12_STATE_SUBOBJECT_TYPE_RAYTRACING_PIPELINE_CONFIG;
+	subobj.pDesc = &m_rtconfig;
+	m_statesubobjects.push_back(subobj);
 }
--- RTPSO.h
+++ RTPSO.h
@@ -8,10 +8,13 @@ public:
 	~RTPSO();
 
 	void Init(ComPtr<ID3D12Device5> creationdevice);
+	void SetPipelineConfig(UINT maxtracerecursiondepth=1);
 
 private:
 	ComPtr<ID3D12StateObject> m_stateobject;
 	D3D12_STATE_OBJECT_DESC m_desc;
 	vector<D3D12_STATE_SUBOBJECT> m_statesubobjects;
+	//only 1 rt config for a pipeline(many does not makes any sense so keep this desc a direct member variable.
+	D3D12_RAYTRACING_PIPELINE_CONFIG m_rtconfig;
 };
 
