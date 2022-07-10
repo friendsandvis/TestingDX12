d289d9504faf85ef35c247450772176304774cf2
Vishwas Srivastava <friendsandvis@gmail.com>
2022-07-10 18:33:28 +0530

    rgs updated to render  a triangle.

--- shaders/raytracing/RT/simplergs2.hlsl
+++ shaders/raytracing/RT/simplergs2.hlsl
@@ -10,11 +10,17 @@ void rgsmain()
 Simpleraypayload payload;
 payload.outcol=float3(0.0f,0.0f,0.0f);
 RayDesc ray;
-ray.Origin=float3(0.0f,0.0f,1.0f);
-ray.TMin=0.01f;
+//ray.Origin=float3(0.0f,0.0,1.0f);
+ray.TMin=0.001f;
 ray.TMax=10.0f;
 ray.Direction=float3(0.0f,0.0f,-1.0f);
 	uint3 rayidx=DispatchRaysIndex();
+	uint3 raydims=DispatchRaysDimensions();
+	float2 offset=rayidx.xy/float2(raydims.xy);
+	offset.y*=-1.0f;
+	//offset-=float2(0.5f,0.5f);
+	//offset*=2.0f;
+	ray.Origin=float3(offset,1.0f);
 	TraceRay(basicas,RAY_FLAG_NONE,0xFF,0,0,0,ray,payload);
 	outtex[rayidx.xy]=float4(payload.outcol,1.0f);
 }
