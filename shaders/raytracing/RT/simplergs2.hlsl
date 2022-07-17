fc7d846320c1291363ffa9203742a6f04db0a694
Vishwas Srivastava <friendsandvis@gmail.com>
2022-07-17 18:12:00 +0530

    fixed a mistake in first triangle raytracing by properly calculationing the rays in rgs.

--- shaders/raytracing/RT/simplergs2.hlsl
+++ shaders/raytracing/RT/simplergs2.hlsl
@@ -1,3 +1,7 @@
+/*
+Notes:
+ray origin is calculated based on the dispatch index and a center(this center value is  what test geometry's verticies  refrence to as center)
+*/
 struct Simpleraypayload
 {
 	float3 outcol;
@@ -10,6 +14,7 @@ void rgsmain()
 Simpleraypayload payload;
 payload.outcol=float3(0.0f,0.0f,0.0f);
 RayDesc ray;
+float2 center=float2(-0.5f,-0.5f);
 //ray.Origin=float3(0.0f,0.0,1.0f);
 ray.TMin=0.001f;
 ray.TMax=10.0f;
@@ -17,10 +22,13 @@ ray.Direction=float3(0.0f,0.0f,-1.0f);
 	uint3 rayidx=DispatchRaysIndex();
 	uint3 raydims=DispatchRaysDimensions();
 	float2 offset=rayidx.xy/float2(raydims.xy);
-	offset.y*=-1.0f;
+	//offset.y*=-1.0f;
 	//offset-=float2(0.5f,0.5f);
 	//offset*=2.0f;
-	ray.Origin=float3(offset,1.0f);
+	ray.Origin=float3(center+offset,1.0f);
 	TraceRay(basicas,RAY_FLAG_NONE,0xFF,0,0,0,ray,payload);
-	outtex[rayidx.xy]=float4(payload.outcol,1.0f);
+	//flipping the output.
+	uint2 outindex=uint2(rayidx.x,raydims.y-rayidx.y);
+	
+	outtex[outindex]=float4(payload.outcol,1.0f);
 }
