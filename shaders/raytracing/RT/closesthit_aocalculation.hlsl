3b7619d28ef1b8493fbf24b7af790259ed7cac0a
Vishwas Srivastava <friendsandvis@gmail.com>
2023-03-12 19:10:00 +0530

    added utility functions for cosinweighted ao ray generation(copied from cwyman ao tutorial) to our aocalc shader.Need to use them and test.

--- shaders/raytracing/RT/closesthit_aocalculation.hlsl
+++ shaders/raytracing/RT/closesthit_aocalculation.hlsl
@@ -17,6 +17,48 @@ struct RTConstantSimple
 RaytracingAccelerationStructure basicas:register(t0);
 ConstantBuffer<RTConstantSimple> rtconstmatrix:register(b1);
 RWTexture2D<float4> gbuffertex[3]: register(u1);
+//util functions for ao ray generation(taken from "http://cwyman.org/code/dxrTutors/tutors/Tutor5/tutorial05")
+uint initRand(uint val0, uint val1, uint backoff = 16)
+{
+	uint v0 = val0, v1 = val1, s0 = 0;
+
+	[unroll]
+	for (uint n = 0; n < backoff; n++)
+	{
+		s0 += 0x9e3779b9;
+		v0 += ((v1 << 4) + 0xa341316c) ^ (v1 + s0) ^ ((v1 >> 5) + 0xc8013ea4);
+		v1 += ((v0 << 4) + 0xad90777d) ^ (v0 + s0) ^ ((v0 >> 5) + 0x7e95761e);
+	}
+	return v0;
+}
+float3 getPerpendicularVector(float3 u)
+{
+	float3 a = abs(u);
+	uint xm = ((a.x - a.y)<0 && (a.x - a.z)<0) ? 1 : 0;
+	uint ym = (a.y - a.z)<0 ? (1 ^ xm) : 0;
+	uint zm = 1 ^ (xm | ym);
+	return cross(u, float3(xm, ym, zm));
+}
+// Takes our seed, updates it, and returns a pseudorandom float in [0..1]
+float nextRand(inout uint s)
+{
+	s = (1664525u * s + 1013904223u);
+	return float(s & 0x00FFFFFF) / float(0x01000000);
+}
+float3 getCosHemisphereSample(inout uint randSeed, float3 hitNorm)
+{
+	// Get 2 random numbers to select our sample with
+	float2 randVal = float2(nextRand(randSeed), nextRand(randSeed));
+
+	// Cosine weighted hemisphere sample from RNG
+	float3 bitangent = getPerpendicularVector(hitNorm);
+	float3 tangent = cross(bitangent, hitNorm);
+	float r = sqrt(randVal.x);
+	float phi = 2.0f * 3.14159265f * randVal.y;
+
+	// Get our cosine-weighted hemisphere lobe sample direction
+	return tangent * (r * cos(phi).x) + bitangent * (r * sin(phi)) + hitNorm.xyz * sqrt(1 - randVal.x);
+}
 [shader("closesthit")]
 void closesthitmain(inout Simpleraypayload payload,in BuiltInTriangleIntersectionAttributes intersectionattribs)
 {
