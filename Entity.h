5efe52e9a7239a6773610bc02521b6d636f10ae3
Vishwas Srivastava <friendsandvis@gmail.com>
2024-01-20 21:27:57 +0530

    added some common functions  to entity class

--- Entity.h
+++ Entity.h
@@ -8,7 +8,8 @@ class Entity
 public:
 	Entity();
 	~Entity();
-
+	std::string GetName() { m_name; }
+	void SetName(std::string name) { m_name = name; }
 private:
 	std::string m_name;
 	DirectX::XMMATRIX m_translatemat, m_rotatemat, scalemat;
